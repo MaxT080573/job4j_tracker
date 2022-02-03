7596a18b90f5565ce3db179a97a5b9a91ebe6720
Kryukov_MM <Nordic54321@mail.ru>
2022-02-03 22:18:52 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.oop;
+
+public class Ball {
+
+    public void tryRun(Boolean condition) {
+        if (condition = false) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
+    }
+
+    public static void main(String[] args) {
+        Ball says = new Ball();
+        says.tryRun(true);
+
+    }
+}
