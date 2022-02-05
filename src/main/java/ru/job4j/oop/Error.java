31c3e1408dc75b3df1eb24cb734e6ba8acadcc77
Kryukov_MM <Nordic54321@mail.ru>
2022-02-06 01:54:19 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error () {
+    }
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main  (String[] args) {
+       Error error  = new Error();
+       Error error2 = new Error(true, 100, "Error 100");
+       Error error3 = new Error(true, 200, "Error 200");
+       error.printInfo();
+       error2.printInfo();
+       error3.printInfo();
+    }
+
+    public void printInfo () {
+            System.out.println("Активность: " + active);
+            System.out.println("Статус: " + status);
+            System.out.println("Cообщение: " + message);
+        }
+}
+
