a7828925ffde1e6e30e16d2051d82f7981cea9d5
Kryukov_MM <Nordic54321@mail.ru>
2022-02-07 19:12:39 +0300

    1.8. Состояние объекта [#167388 #261269]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,16 +3,16 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
-    public Battery (int load) {
+    public Battery(int load) {
         this.load = load;
     }
 
-    public int getLoad () {
+    public int getLoad() {
         return load;
 
     }
 
-    public void exchange (Battery another) {
+    public void exchange(Battery another) {
         another.load = this.load + another.load;
         this.load = 0;
 
