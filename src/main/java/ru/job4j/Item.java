0149bcffba04257c49388fab1e1d35f387dab478
MaxT080573 <Nordic54321@mail.ru>
2022-04-18 14:20:39 +0300

    4. Переопределение [#174890 #289268]

--- src/main/java/ru/job4j/Item.java
+++ src/main/java/ru/job4j/Item.java
@@ -1,35 +1,44 @@
 package ru.job4j;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
 
-    private int id;
-    private String name;
+    LocalDateTime created = LocalDateTime.now();
+
+        public LocalDateTime getCreated () {
+            return created;
+        }
+
+        private int id;
+        private String name;
 
     public Item() {
-    }
+        }
 
     public Item(String name) {
-        this.name = name;
-    }
+            this.name = name;
+        }
 
-    public Item(int id, String name) {
-        this.name = name;
-        this.id = id;
-    }
+    public Item( int id, String name){
+            this.name = name;
+            this.id = id;
+        }
 
-    public int getId() {
-        return id;
-    }
+        public int getId () {
+            return id;
+        }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+        public void setId ( int id){
+            this.id = id;
+        }
 
-    public String getName() {
-        return name;
-    }
+        public String getName () {
+            return name;
+        }
 
-    public void setName(String name) {
-        this.name = name;
+        public void setName (String name){
+            this.name = name;
+        }
     }
-}
