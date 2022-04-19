703651bebea16fd058c17b084fd0b00d7a422604
MaxT080573 <Nordic54321@mail.ru>
2022-04-19 15:06:56 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/Item.java
+++ src/main/java/ru/job4j/Item.java
@@ -25,19 +25,19 @@ public class Item {
             this.id = id;
         }
 
-        public int getId() {
+    public int getId() {
             return id;
         }
 
-        public void setId(int id) {
+    public void setId(int id) {
             this.id = id;
         }
 
-        public String getName() {
+    public String getName() {
             return name;
         }
 
-        public void setName(String name) {
+    public void setName(String name) {
             this.name = name;
         }
     }
