c33af69ebce476f2befb92b81c2ead66f564fc34
MaxT080573 <Nordic54321@mail.ru>
2022-04-20 18:15:44 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class Item {
+
+   private int id;
+   private String name;
+   private LocalDateTime created = LocalDateTime.now();
+
+
+   public Item() {
+   }
+
+   public Item(String name) {
+      this.name = name;
+   }
+
+   public Item(int id, String name) {
+      this.name = name;
+      this.id = id;
+   }
+
+   public LocalDateTime getCreated() {
+      return created;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
