ae09c5666af7b5e4712a95d5785a718fc50580a5
MaxT080573 <Nordic54321@mail.ru>
2022-04-26 17:28:27 +0300

    2. Модель данных. [#174683]

---
+++ src/test/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main (String[] args) {
+        Student student = new Student();
+        student.setName("Smirnov Ivan Petrovich ");
+        student.setGroup("ER-87-1 ");
+        student.setDate("27/07/1987");
+        System.out.println(student.getName() + student.getGroup() +  student.getDate());
+    }
+}
---
+++ src/test/java/ru/job4j/threads/Main.java
@@ -0,0 +1,43 @@
+package ru.job4j.threads;
+
+public class Main{
+
+    public static void main (String[] args) throws Exception {
+        ThreadOne myThread = new ThreadOne();
+        ThreadTwo myThread2 = new ThreadTwo();
+
+        myThread.start();
+        myThread2.start();
+
+    }
+}
+
+class ThreadOne extends Thread implements Runnable{
+
+    @Override
+    public void run () {
+        while (true) {
+            System.out.println("поток 1");
+            try {
+                sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class ThreadTwo extends Thread implements Runnable{
+
+    @Override
+    public void run () {
+        while (true) {
+            System.out.println("поток 2");
+            try {
+                sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
