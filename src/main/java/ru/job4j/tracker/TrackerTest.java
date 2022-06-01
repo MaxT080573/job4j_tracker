2d17a0a1848324abf7ec7e56f435d49d6d49cf53
MaxT080573 <Nordic54321@mail.ru>
2022-06-01 23:22:28 +0300

    5. Tracker - хранилище [#396 #307013]

--- src/main/java/ru/job4j/tracker/TrackerTest.java
+++ src/main/java/ru/job4j/tracker/TrackerTest.java
@@ -16,19 +16,6 @@ public class TrackerTest {
     }
 
     @Test
-    public void whenReplace() {
-        Tracker tracker = new Tracker();
-        Item bug = new Item();
-        bug.setName("Bug");
-        tracker.add(bug);
-        int id = bug.getId();
-        Item bugWithDesc = new Item();
-        bugWithDesc.setName("Bug with description");
-        tracker.replace(id, bugWithDesc);
-        assertThat(tracker.findById(id).getName(), is("Bug with description"));
-    }
-
-    @Test
     public void whenTestFindAll() {
         Tracker tracker = new Tracker();
         Item first = new Item("First");
